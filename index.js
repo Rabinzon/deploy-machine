cc3681375aa1a3c08dd7d335bf1a56b8c8a445e3
rabinzon <ildarrazin@gmail.com>
2016-11-18 00:37:55 +0300

    fix: post

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const child_process = require('child_process');
 const conf = require('./conf.json');
 const app = express();
 
-app.get(`/${process.env.URL}`, (req, res) => {
+app.post(`/${process.env.URL}`, (req, res) => {
 	child_process.exec(process.env.COMMAND);
 	res.send('OK');
 });
