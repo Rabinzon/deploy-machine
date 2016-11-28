d05fe528c34197589bc74ba4a02c5fc7eb4bf290
rabinzon <ildarrazin@gmail.com>
2016-11-28 22:07:51 +0300

    feat: add config & building handlers from config

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
 const express = require('express');
 const child_process = require('child_process');
-const app = express();
 
-app.post(`/${process.env.URL}`, (req, res) => {
-	child_process.exec(process.env.COMMAND);
-	res.send('OK');
+const app = express();
+const file = fs.readFileSync(`${process.env.CONF}`, {encoding: 'utf-8'});
+const config = yaml.load(file);
+
+config.forEach(item => {
+	app.post(`/${item.url}`, (req, res) => {
+		child_process.exec(item.exec);
+		res.send('OK');
+	});
 });
 
 app.listen(3314);
